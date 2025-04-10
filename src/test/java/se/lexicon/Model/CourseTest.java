2c2aeef2260b39e613377eff7a4131bcbd6541e7
Dennis Olsen <dennan_lol@hotmail.com>
2025-04-10 11:44:17 +0200

    Add Student and Course unit tests

--- src/test/java/se/lexicon/Model/CourseTest.java
+++ src/test/java/se/lexicon/Model/CourseTest.java
@@ -1,4 +1,90 @@
 package se.lexicon.Model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CourseTest {
+
+    @Test
+    public void testCreatingACourse_Success(){
+        // expected
+        String courseName = "Java Programming";
+        LocalDate startDate = LocalDate.now().plusWeeks(5);
+        int weekDuration = 5;
+
+        // actual
+        Course actual = new Course("Java Programming", LocalDate.now().plusWeeks(5), 5);
+
+        // verify
+        Assertions.assertNotNull(actual);
+        Assertions.assertNotNull(actual.getStudents());
+        Assertions.assertEquals(0, actual.getStudents().size());
+        Assertions.assertEquals(courseName, actual.getCourseName());
+        Assertions.assertEquals(startDate, actual.getStartDate());
+        Assertions.assertEquals(weekDuration, actual.getWeekDuration());
+    }
+
+    @Test
+    public void testRegisterStudent_Success() {
+        Course course = new Course("Java Programming", LocalDate.now().plusWeeks(5), 5);
+        Student student = new Student("Dennis", "dennis@test.se", "newglrt19");
+
+        // expected
+        Student expected = new Student("Dennis", "dennis@test.se", "newglrt19");
+
+        // actual
+        course.register(student);
+        try{
+            Student actual = course.getStudents().get(0);
+
+            // verify
+            Assertions.assertEquals(expected.getName(), actual.getName());
+            Assertions.assertEquals(expected.getEmail(), actual.getEmail());
+            Assertions.assertEquals(expected.getAddress(), actual.getAddress());
+        }
+        catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
+
+    @Test
+    public void testRegisterStudent_Duplicate_AddFails(){
+        Course course = new Course("Java Programming", LocalDate.now().plusWeeks(5), 5);
+        Student student = new Student("Dennis", "dennis@test.se", "newglrt19");
+        course.register(student);
+
+        // Expected:
+        String expected = "Error (Course.java): Duplicate id, duplicate students not allowed.";
+
+        // Actual:
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> course.register(student));
+        String actual = exception.getMessage();
+
+        // Verify the result:
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUnRegisterStudent_Success() {
+        Course course = new Course("Java Programming", LocalDate.now().plusWeeks(5), 5);
+        Student student = new Student("Dennis", "dennis@test.se", "newglrt19");
+        course.register(student);
+
+        // expected
+        int sizeBefore = course.getStudents().size();
+
+        // actual
+        course.unRegister(student);
+        int sizeAfter = course.getStudents().size();
+
+        // verify
+        Assertions.assertEquals(1, sizeBefore);
+        Assertions.assertEquals(0, sizeAfter);
+    }
 }
--- src/test/java/se/lexicon/Model/StudentTest.java
+++ src/test/java/se/lexicon/Model/StudentTest.java
@@ -1,4 +1,26 @@
 package se.lexicon.Model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
+    @Test
+    public void testCreatingStudent_Success(){
+
+        // expected
+        int id = 1;
+        String name = "Dennis";
+        String email = "dennis@test.se";
+        String address = "newglrt19";
+
+        // actual
+        Student actual = new Student( "Dennis", "dennis@test.se", "newglrt19");
+
+        // verify
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(id, actual.getId());
+        Assertions.assertEquals(name, actual.getName());
+        Assertions.assertEquals(email, actual.getEmail());
+        Assertions.assertEquals(address, actual.getAddress());
+    }
 }
